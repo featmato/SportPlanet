a92449861a7ec7cc0c0116c6e2cecdb3defe9d1a
featmato <135019784+featmato@users.noreply.github.com>
2023-06-01 09:15:16 +0200

    Add files via upload

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
-var sports = document.getElementById("sports")
+/*var sports = document.getElementById("sports")
 
 var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", "611e21b7a64f6062ccb6fd1f40414e4c");
+myHeaders.append("x-rapidapi-key", "");
 myHeaders.append("x-rapidapi-host", "v1.hockey.api-sports.io");
 
 var requestOptions = {
@@ -14,3 +14,35 @@ fetch("https://v1.hockey.api-sports.io/countries", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+*/
+
+const apiKey = ''; // Replace with your API key
+const apiUrl = 'v1.hockey.api-sports.io'; // Replace with the API URL
+
+// Function to fetch live scores
+async function getLiveScores() {
+  try {
+    const response = await fetch(apiUrl, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+      },
+    });
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch live scores');
+    }
+    
+    const data = await response.json();
+    
+    // Process the live scores data
+    // ...
+    
+    console.log(data); // Log the live scores data to the console
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the function to fetch live scores
+getLiveScores();
